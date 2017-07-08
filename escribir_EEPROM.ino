695f9c74cd1018d5586672f358f748be9ae26eba
diegotestereo <diegotestereo@gmail.com>
2017-07-08 16:31:08 -0300

    fix
    se completo la memoria actualizada con varias direcciones adicionales

--- escribir_EEPROM.ino
+++ escribir_EEPROM.ino
@@ -2,37 +2,76 @@
 
 char ssid[20];
 char pass[20];
-char Topic1[20];
-char Topic2[20];
-char ServerWan[40];
-char ServerLan[20];
+char Topic1[30];
+char Topic2[30];
+char TopicSensor[30];
+char TopicPir[30];
+char ServerWan[30];
+char ServerLan[30];
+char Home[20];
+char Floor[20];
+char Site[20];
+char Id[20];
+char Department[20];
+char Port[5];
+char UserMqtt[20];
+char PassMqtt[20];
 
 String ssid_leido;
 String pass_leido;
+String Home_leido;
+String Floor_leido;
+String Site_leido;
+String Id_leido;
+String Department_leido;
 String Topic1_leido;
 String Topic2_leido;
+String TopicSensor_leido;
+String TopicPir_leido;
 String ServerWan_leido;
 String ServerLan_leido;
+String Port_leido;
+String UserMqtt_leido;
+String PassMqtt_leido;
 
 String scanWifi;
 
 int ssid_tamano = 0;
 int pass_tamano = 0;
+int Home_tamano;
+int Floor_tamano;
+int Site_tamano;
+int Id_tamano;
+int Department_tamano;
 int Topic1_tamano = 0;
 int Topic2_tamano = 0;
+int TopicSensor_tamano = 0;
+int TopicPir_tamano = 0;
 int ServerWan_tamano = 0;
 int ServerLan_tamano = 0;
+int Port_tamano = 0;
+int UserMqtt_tamano = 0;
+int PassMqtt_tamano = 0;
 
 ////// ADDRESS EEPROM
-int dir_modo= 0; // 0=normal 1 configuracion
-int dir_conf = 70;
+int dir_modo= 0; //0=normal 1 configuracion
 int dir_ssid = 1;
 int dir_pass = 30;
+int dir_conf = 70;
 int dir_topic1 = 100;
 int dir_topic2 = 130;
-int dir_serverwan = 150;
-int dir_serverlan = 180;
-int dir_puerto = 210;
+int dir_serverwan = 160;
+int dir_serverlan = 190;
+int dir_puerto = 220;
+int dir_topic_sensor = 230;
+int dir_topic_pir = 260;
+int dir_department= 280;
+int dir_home =300;
+int dir_floor =320;
+int dir_site =340;
+int dir_id=360;
+int dir_userMqtt=380;
+int dir_passMqtt=400;
 
 String arregla_simbolos(String a) {
   a.replace("%C3%A1", "Ã¡");
@@ -68,8 +107,7 @@ void setup() {
 
   EEPROM.begin(512);
   delay(10);
-  WriteDataEprom();
-  
+  WriteDataEprom(); 
 
 }
 
@@ -108,36 +146,53 @@ String lee(int addr) {
 void WriteDataEprom(){
  
   graba(dir_modo,"0");
+  graba(dir_conf,"configurado");
+  
   graba(dir_ssid,"Consola");
   graba(dir_pass,"tyrrenal");
-  graba(dir_conf,"configurado");
-  graba(dir_topic1,"prueba/light1");
-  graba(dir_topic2,"prueba/light2");
   graba(dir_serverwan,"idirect.dlinkddns.com");
   graba(dir_serverlan,"192.168.1.106");
   graba(dir_puerto,"1883");
-/*
-  graba(dir_modo,"0");
-  graba(dir_ssid,"");
-  graba(dir_pass,"");
-  graba(dir_conf,"");
-  graba(dir_topic1,"");
-  graba(dir_topic2,"");
-  graba(dir_serverwan,"");
-  graba(dir_serverlan,"");
-  graba(dir_puerto,"");
-   */
- 
-  Serial.println(lee(0)); 
-  Serial.println(lee(1));
-  Serial.println(lee(30));
-  Serial.println(lee(70));
-  Serial.println(lee(100));
-  Serial.println(lee(130));
-  Serial.println(lee(150));
-  Serial.println(lee(180));
-  Serial.println(lee(210));
 
+  graba(dir_home, "casa");
+  graba(dir_floor, "piso-0");
+  graba(dir_department, "dto-0");
+  graba(dir_site, "living");  
   
+  graba(dir_id, "esp-00");
+  graba(dir_topic1,"light1");
+  graba(dir_topic2,"light2");
+  graba(dir_topic_sensor, "dht11");
+  graba(dir_topic_pir, "pir");
+ 
+  graba(dir_userMqtt, "diego");
+  graba(dir_passMqtt, "24305314");
+
+
+  Serial.println("Lectura"); 
+  Serial.println();
+  Serial.println(lee(dir_modo)); 
+  Serial.println(lee(dir_conf));
   
+  Serial.println(lee(dir_ssid));
+  Serial.println(lee(dir_pass));
+  Serial.println(lee(dir_serverwan));
+  Serial.println(lee(dir_serverlan));
+  Serial.println(lee(dir_puerto));
+
+  Serial.println(lee(dir_home));
+  Serial.println(lee(dir_floor));
+  Serial.println(lee(dir_department));
+  Serial.println(lee(dir_site));
+  
+  Serial.println(lee(dir_id));
+  Serial.println(lee(dir_topic1));
+  Serial.println(lee(dir_topic2));
+  Serial.println(lee(dir_topic_sensor));
+  Serial.println(lee(dir_topic_pir));
+
+  Serial.println(lee(dir_userMqtt));
+  Serial.println(lee(dir_passMqtt));
+
+   
   }
